ba188359375687400d95bd592e97fd59b95c7b0e
Connor Imes <connor.k.imes@gmail.com>
2015-08-03 09:06:17 -0500

    Name refactoring in native lib

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,9 @@ extern crate libc;
 extern crate energymon_sys;
 
 use libc::{c_int};
-use energymon_sys::em_impl;
+use energymon_sys::energymon;
 
 extern "C" {
-    /// Native C function that fills in the em_impl struct values and may allocate other resources.
-    pub fn em_impl_get(em: *mut em_impl) -> c_int;
+    /// Native C function that fills in the energymon struct values.
+    pub fn energymon_get_default(em: *mut energymon) -> c_int;
 }
